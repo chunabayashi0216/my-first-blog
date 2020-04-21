53f6593559627e5c138c83d8383622417b288a97
Your Name <you@example.com>
2020-04-21 16:50:05 +0900

    Changed the HTML for the site.

--- mymenve/bin/activate.fish
+++ mymenve/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/mc/djangogirls/myvenv（）"
+set -gx VIRTUAL_ENV "/home/mc/djangogirls/mymenve"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
@@ -52,8 +52,8 @@ if test -z "$VIRTUAL_ENV_DISABLE_PROMPT"
         set -l old_status $status
 
         # Prompt override?
-        if test -n "(myvenv（）) "
-            printf "%s%s" "(myvenv（）) " (set_color normal)
+        if test -n "(mymenve) "
+            printf "%s%s" "(mymenve) " (set_color normal)
         else
             # ...Otherwise, prepend env
             set -l _checkbase (basename "$VIRTUAL_ENV")
